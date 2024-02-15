13417b8bddbf2895c1c77792c319efda03ea7828
Stefan Westerfeld <stefan@space.twc.de>
2024-02-15 11:36:13 +0100

    ase/lv2device.cc: improve the documentation for async preset loading
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- ase/lv2device.cc
+++ ase/lv2device.cc
@@ -2461,9 +2461,9 @@ PluginInstance::restore_preset (int preset, PortRestoreHelper *helper)
 
 /*
  * Asynchronous preset loading: a user can select a new preset in the UI -
- * since this is a choice propery of the Processor, the audio thread will get
- * notified during render() that a preset should be loaded, but we don't want
- * to stall the audio thread to actually load the preset (which can take a
+ * since this is a choice propery of the LV2Processor, the audio thread will
+ * get notified during render() that a preset should be loaded, but we don't
+ * want to stall the audio thread to actually load the preset (which can take a
  * while, depending on the plugin). See also #37.
  *
  * So loading a preset works like this:
@@ -2479,8 +2479,8 @@ PluginInstance::restore_preset (int preset, PortRestoreHelper *helper)
  *
  * Since LV2 does not allow preset loading and calling the run function at the
  * same time, the AudioProcessor checks if the std::atomic contains
- * PRESET_STATE_READY before calling the run function. If it does, it does not
- * call the run() function and fills all output buffers with zeros.
+ * PRESET_STATE_READY before calling the run function. Only if it does, it calls
+ * the run() function. Otherwise, it fills all output buffers with zeros.
  */
 void
 PluginInstance::restore_preset_async (int preset)
