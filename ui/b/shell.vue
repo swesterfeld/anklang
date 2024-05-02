bc11e7d653ce3905f29fc8bdc4b2c167ba11a01d
Tim Janik <timj@gnu.org>
2024-05-02 21:45:01 +0200

    ui/b/shell.vue: pass `existing` to b-crawlerdialog, default to ~MUSIC
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ui/b/shell.vue
+++ ui/b/shell.vue
@@ -143,7 +143,7 @@ html.b-shell-during-drag .b-app {
     <!-- Modal Dialogs -->
     <b-aboutdialog :shown.prop="show_about_dialog_" @close="show_about_dialog(0)" v-if="show_about_dialog_" />
     <b-crawlerdialog v-if="!!fs.resolve" :shown.prop="true" :title="fs.title" :filters="fs.filters" :button="fs.button"
-		     :cwd="fs.cwd" @close="fs.resolve()" @select="fs.resolve($event.detail.uri)" />
+		     :existing.prop="fs.existing" :cwd="fs.cwd" @close="fs.resolve()" @select="fs.resolve($event.detail?.uri)" />
     <div class="-fullcoverage" style="z-index: 90" id="b-app-shell-modaldialogs" >
       <b-preferencesdialog v-model:shown="Data.show_preferences_dialog" />
 
@@ -212,7 +212,7 @@ class ShellClass extends Envue.Component {
   data_bubble = null;
   constructor (vm) {
     super (vm);
-    this.fs = Vue.reactive ({ title: 'File Selector', button: 'Select', cwd: 'MUSIC', filters: [], resolve: null });
+    this.fs = Vue.reactive ({ title: 'File Selector', button: 'Select', cwd: '~MUSIC', filters: [], resolve: null });
     this.m = observable_project_data.call (vm);
     this.m.modal_dialogs = [];
     this.m.show_spinner_count = 0;
@@ -339,10 +339,11 @@ class ShellClass extends Envue.Component {
     if (this.fs.resolve)
       return undefined;
     Object.assign (this.fs, opt);
+    this.fs.existing === false || (this.fs.existing = true);
     let resolve;
     const fileselector_promise = new Promise (r => resolve = r);
     this.fs.resolve = path => { // assignment shows file selector
-      this.fs.resolve = null;   // assignment hides file selector
+      this.fs.resolve = null;   // reset hides file selector
       resolve (path);
     };
     return await fileselector_promise;
