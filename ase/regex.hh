9d4e6230c47b37a51c09cbff50edbefb8cc17d88
Tim Janik <timj@gnu.org>
2024-06-14 03:38:43 +0200

    ase/regex: add Re::grep() with Perl regex semantics
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ase/regex.hh
+++ ase/regex.hh
@@ -11,13 +11,19 @@ class Re final {
 public:
   enum Flags : int32_t {
     DEFAULT     = 0,
-    ERE         = 0x01, // Posix Extended
-    I           = 0x10, // IGNORECASE
-    // M        = 0x20, // MULTILINE
-    // S        = 0, DOTALL not supported, use [\s\S] or [^\x0]
+    ERE         = 1 <<  0, // Posix Extended
+    I           = 1 <<  4, // IGNORECASE
+    M           = 1 <<  5, // MULTILINE
+    N           = 1 <<  6, // NO_AUTO_CAPTURE
+    S           = 1 <<  7, // DOTALL
+    X           = 1 <<  8, // EXTENDED
+    XX          = 1 <<  9, // EXTENDED_MORE
+    J           = 1 << 10, // DUPNAMES
+    U           = 1 << 11, // UNGREEDY
   };
   static StringS findall (const String &regex, const String &input, Flags = DEFAULT);
   static ssize_t search  (const String &regex, const String &input, Flags = DEFAULT);
+  static String  grep    (const String &regex, const String &input, int group = 0, Flags = DEFAULT);
   static String  subn    (const String &regex, const String &subst, const String &input, uint count = 0, Flags = DEFAULT);
   static String  sub     (const String &regex, const String &sbref, const String &input, Flags = DEFAULT);
 };
