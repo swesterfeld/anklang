d658f1dda0e5cecee7de6930907bcd30548f883e
Tim Janik <timj@gnu.org>
2024-06-15 04:22:16 +0200

    x11test/ereplay.mjs: add replay handling for *.json files
    * Support replay of multiple *.json files in succession.
    * Add extra delay in case of page reload.
    * Adjust delays around clicks.
    * Raise maximum timeout, only triggered if something went wrong.
    
    Signed-off-by: Tim Janik <timj@gnu.org>

--- x11test/ereplay.mjs
+++ x11test/ereplay.mjs
@@ -4,12 +4,15 @@
 // constants
 const verbose = process.argv.indexOf ('--verbose') >= 0;
 loginf ("startup", "verbose=" + verbose);
+const quiet = process.argv.indexOf ('--quiet') >= 0;
 const devtools = process.argv.indexOf ('--devtools') >= 0;
 const localhost = "127.0.0.1";
 
 // == imports ==
 loginf ("import puppeteer-core");
 import puppeteer from "puppeteer-core";
+loginf ("import @puppeteer/replay");
+import { PuppeteerRunnerExtension, createRunner, parse } from "@puppeteer/replay";
 loginf ("import electron");
 import { app, BrowserWindow } from "electron";
 loginf ("import get-port");
@@ -73,7 +76,8 @@ async function puppeteer_connect (listenhost, port)
   //loginf ("response object:", response_object);
   loginf ("response webSocketDebuggerUrl:", response_object.webSocketDebuggerUrl);
   loginf ("await puppeteer.connect");
-  const puppeteer_browser = await puppeteer.connect ({ browserWSEndpoint: response_object.webSocketDebuggerUrl, defaultViewport: null });
+  const puppeteer_browser = await puppeteer.connect ({ browserWSEndpoint: response_object.webSocketDebuggerUrl,
+						       defaultViewport: { width: 1920, height: 1080 } });
   loginf ("browser:", puppeteer_browser);
   // monkey patch puppeteer_browser.newPage which otherwise cannot work
   puppeteer_browser.newPage = puppeteer_newpage.bind (puppeteer_browser);
@@ -131,6 +135,76 @@ async function puppeteer_newpage (settings = {})
   return puppeteer_getpage (this, window);
 }
 
+// == abort on any errors ==
+function test_error (arg) {
+  console.error (arg, "\nAborting...");
+  app.exit (255);
+  process.abort();
+}
+process.on ('uncaughtException', test_error);
+process.on ('unhandledRejection', test_error);
+
+// == delay ==
+const delay = ms => new Promise (resolve => setTimeout (resolve, ms));
+
+// == TestRunnerExtension ==
+const MAX_TIMEOUT = 10 * 1000;
+const BEFORE_CLICK = 50;	// give time to let clickable elements emerge
+const CLICK_DURATION = 99; 	// allow clicks to have a UI effect
+const AFTER_CLICK = 50;		// give time to let clicks take effect
+const RELOAD_DELAY = 1500;
+const DELAY_EXIT = 1000;
+class UnhurriedTestRunnerExtension extends PuppeteerRunnerExtension {
+  constructor (browser, page, opts, scriptname)
+  {
+    super (browser, page, opts);
+    this.page = page;
+    this.scriptname = scriptname;
+    this.log = (...args) => quiet || console.log (this.scriptname + ":", ...args);
+  }
+  async beforeAllSteps (flow)
+  {
+    await super.beforeAllSteps (flow);
+    if (!quiet) console.log ("\nSTART REPLAY:", this.scriptname);
+  }
+  async beforeEachStep (step, flow)
+  {
+    if (click_types.indexOf (step.type) >= 0 && !step.duration)
+      step.duration = CLICK_DURATION;
+    const s = step.selectors ? step.selectors.flat() : [];
+    let msg = step.type;
+    if (step.url)
+      msg += ': ' + step.url;
+    else if (s.length)
+      msg += ': ' + s[0];
+    else
+      msg += '...';
+    this.log (msg);
+    if (click_types.indexOf (step.type) >= 0)
+      await delay (BEFORE_CLICK);
+    await super.beforeEachStep (step, flow);
+  }
+  async afterEachStep (step, flow)
+  {
+    await super.afterEachStep (step, flow);
+    if (click_types.indexOf (step.type) >= 0)
+      await delay (AFTER_CLICK);
+    const s = step.selectors ? step.selectors.flat() : [];
+    const navigate = "navigate" === step.type || s[0]?.search (/\bNew.?Project/i) >= 0;
+    if (navigate) { // Anklang may cause a page reload on "navigate" events
+      this.log ("  reload delay...");
+      await delay (RELOAD_DELAY);
+    }
+  }
+  async afterAllSteps (flow)
+  {
+    await super.afterAllSteps(flow);
+    if (!quiet) console.log ("SUCCESS!");
+    await delay (DELAY_EXIT);	// time for observer
+  }
+}
+const click_types = [ 'click', 'doubleClick' ];
+
 // == main ==
 async function main (argv)
 {
@@ -143,13 +217,31 @@ async function main (argv)
   loginf ("await puppeteer_connect");
   const puppeteer_browser = await puppeteer_connect (localhost, port);
 
-  // https://developer.chrome.com/docs/puppeteer/get-started#save_a_screenshot
-  const page = await puppeteer_browser.newPage (); // { show: false }
-  await page.setViewport ({ deviceScaleFactor: 1, isMobile: false, width: 1080, height: 1920 });
-  await page.goto ('https://example.com', { waitUntil: 'networkidle2', });
-  await page.screenshot({ path: 'example.png', fullPage: true });
-  if (!devtools)
-    await page.close(); // puppeteer_browser.close();
+  // run *.json scripts
+  const show = true;
+  const viewport = { deviceScaleFactor: 1, isMobile: false, width: 1920, height: 1080 };
+  for (const arg of process.argv) {
+    if (!arg.endsWith ('.json')) continue;
+    const json_events = JSON.parse (fs.readFileSync (arg, "utf8"));
+    const page = await puppeteer_browser.newPage ({
+      show,
+      x:                                0,
+      y:                                0,
+      width:                            1920, // calling win.maximize() causes flicker
+      height:                           1080, // using a big initial size avoids flickering
+      backgroundColor:                  '#000',
+      autoHideMenuBar:                  true,
+    });
+    await page.setViewport (viewport);
+
+    page.setDefaultTimeout (MAX_TIMEOUT);
+    const runner_extension = new UnhurriedTestRunnerExtension (puppeteer_browser, page, { timeout: MAX_TIMEOUT }, arg);
+    const runner = await createRunner (parse (json_events), runner_extension);
+    await runner.run();
+
+    if (!devtools)
+      await page.close();
+  }
 
   // shutdown
   loginf ("await window closed");
