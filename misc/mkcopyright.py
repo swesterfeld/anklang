f48b6eb18d6e59de4c3037e0bcd74294e88996d8
Tim Janik <timj@gnu.org>
2024-06-27 20:40:09 +0200

    misc/mkcopyright.py: match Unlicense dedications
    Signed-off-by: Tim Janik <timj@gnu.org>

--- misc/mkcopyright.py
+++ misc/mkcopyright.py
@@ -15,6 +15,8 @@ licenses = {
                   'CC0 Public Domain: http://creativecommons.org/publicdomain/zero/1\.0/',
                   'Licensed CC0 Public Domain: http://creativecommons.org/publicdomain/zero/1\.0' ],
   'MIT':        [ 'MIT [Ll]icensed?', ],
+  'Unlicense':  [ '([Dd]edicated to|[Ii]n) the Public Domain under the Unlicense([: ]*http.*)?',
+                  'Licensed Public Domain' ],
 }
 
 # Comment suffix to ignore
@@ -185,6 +187,7 @@ def print_help (arg0, exit = None):
   h  = "Scan FILES for licenses and list copyrights from Git(1) authors.\n"
   h += "OPTIONS:\n"
   h += "  -b            Display brief license list\n"
+  h += "  -c<inifile>   Read config file in INI format\n"
   h += "  -e            Exit with a non-zero status if any files are unlicensed\n"
   h += "  -h, --help    Show command help\n"
   h += "  -l            List licensed files only\n"
